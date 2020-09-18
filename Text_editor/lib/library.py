bdcdeeab2d5211ee12d2f2c5196004f55a7a6d07
khadj-abdelkader <61092643+khadj-abdelkader@users.noreply.github.com>
2020-09-18 21:51:11 +0200

    Initial commit
    Initial commit

---
+++ Text_editor/lib/library.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import os
+
+class textEditor
+	def__init__(self, master, content):
+	self.master = master
+	self.content = content
+
+def create(self):
+	self.master = Tk()
+	self.master.title("Text Editor")
+	self.master.geometry("700*400") 
+def add_text(self):
+	self.content = Text(self.master)
+	self.content.pack(expand = True, fill = 'both')
+
+def generate(self):
+	self.master.mainloop()
+
---
+++ Text_editor/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import os
+
+from lib.library import *
+
+root = textEditor("window" , "content")
+root.create()
+root.add_text()
+root.generate()
